9c5c4d240db283fed5c1a92ab92c91d566d48321
BluThaitanium <phuthai450@gmail.com>
2022-10-27 22:26:17 -0400

    initial commit

---
+++ .next/static/webpack/app/page.b8c7e44bd3486a4a.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fapp%2Fpage.module.css&server=false!":
+/*!********************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fapp%2Fpage.module.css&server=false! ***!
+  \********************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"./node_modules/next/dist/client/image.js\", 23));\n(() => __webpack_require__.e(/*! import() */ \"app_page_module_css\").then(__webpack_require__.t.bind(__webpack_require__, /*! ./app/page.module.css */ \"./app/page.module.css\", 23)))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2J1aWxkL3dlYnBhY2svbG9hZGVycy9uZXh0LWZsaWdodC1jbGllbnQtZW50cnktbG9hZGVyLmpzP21vZHVsZXM9JTJGVXNlcnMlMkZ0aGFpcCUyRkRlc2t0b3AlMkZUdXJib1BhY2slMkZhcmNoaWlfY3JhbmVfdHVyYm9wYWNrJTJGbm9kZV9tb2R1bGVzJTJGbmV4dCUyRmRpc3QlMkZjbGllbnQlMkZpbWFnZS5qcyZtb2R1bGVzPSUyRlVzZXJzJTJGdGhhaXAlMkZEZXNrdG9wJTJGVHVyYm9QYWNrJTJGYXJjaGlpX2NyYW5lX3R1cmJvcGFjayUyRmFwcCUyRnBhZ2UubW9kdWxlLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLDRMQUFpSTtBQUNqSSxPQUFPLDRLQUE2RyIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzU3ZjciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCIvVXNlcnMvdGhhaXAvRGVza3RvcC9UdXJib1BhY2svYXJjaGlpX2NyYW5lX3R1cmJvcGFjay9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2NsaWVudC9pbWFnZS5qc1wiKTtcbigoKSA9PiBpbXBvcnQoLyogd2VicGFja01vZGU6IFwibGF6eVwiICovIFwiL1VzZXJzL3RoYWlwL0Rlc2t0b3AvVHVyYm9QYWNrL2FyY2hpaV9jcmFuZV90dXJib3BhY2svYXBwL3BhZ2UubW9kdWxlLmNzc1wiKSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Fthaip%2FDesktop%2FTurboPack%2Farchii_crane_turbopack%2Fapp%2Fpage.module.css&server=false!\n"));
+
+/***/ })
+
+});
