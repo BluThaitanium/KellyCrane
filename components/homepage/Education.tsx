ae20b06c3f7209ead94f134f47aae93fc16b6736
BluThaitanium <phuthai450@gmail.com>
2022-12-25 01:53:01 -0500

    shiny degree

--- components/homepage/Education.tsx
+++ components/homepage/Education.tsx
@@ -28,24 +28,31 @@ const Education = ({
   major: string
 }) => {
   return (
-    <div className="bg-[#F0EEE9] w-[90vmin] h-[57vmin] mx-auto py-[2vmin] border-[2vmin] border-x-yellow-800 border-y-orange-900">
-      <div className="text-center w-full">
-        <h2 className={`${pirata.className} text-[7vmin] leading-tight`}>{university}</h2>
-        <div className="flex justify-center items-center text-center">
-          <p className="text-[4vmin]">{degreeMonth}</p>
-          <Image
-            src={logo}
-            alt={degree}
-            width={400}
-            height={400}
-            className="w-[20%] mx-[5vmin]"
-          />
-          <p className="text-[4vmin]">{degreeYear}</p>
+    <div>
+    <div className='relative float-left bg-gradient-to-t from-[#EDDDC2] to-[#FBEBD2] h-[57vmin] w-[calc((100%-90vmin)/2)] m-0 p-0 z-10'></div>
+    <div className='relative float-right bg-gradient-to-t from-[#EDDDC2] to-[#FBEBD2] h-[57vmin] w-[calc((100%-90vmin)/2)] m-0 p-0 z-10'></div>
+    <div className="relative bg-[#F0EEE9] w-[90vmin] h-[57vmin] mx-auto py-[2vmin] border-[2vmin] border-x-yellow-800 border-y-orange-900">
+      <div className='custom-degree-shine'></div>
+        <div className="text-center w-full">
+          <h2 className={`${pirata.className} text-[7vmin] leading-tight`}>{university}</h2>
+          <div className="flex justify-center items-center text-center">
+            <p className="text-[4vmin]">{degreeMonth}</p>
+            <Image
+              src={logo}
+              alt={degree}
+              width={400}
+              height={400}
+              className="w-[20%] mx-[5vmin]"
+            />
+            <p className="text-[4vmin]">{degreeYear}</p>
+          </div>
+          <p className="text-[6vmin]">{degree}</p>
+          <p className="text-[5vmin]">{major}</p>
         </div>
-        <p className="text-[6vmin]">{degree}</p>
-        <p className="text-[5vmin]">{major}</p>
-      </div>
     </div>
+
+    </div>
+
   )
 }
 
